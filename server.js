773bc9858fed1634404c3576493dc23384137c82
bhavBains <deepbains26@gmail.com>
2018-10-25 12:28:46 -0700

    server is up and running with views

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.listen(8080, () => {
+  console.log("Your app is running at PORT 8080");
+});
+
+app.get('/', (req,res) => {
+  res.render('index');
+});
+
+app.get('/grid', (req,res) => {
+  res.render('grid');
+});
+
+app.get('/form', (req,res) => {
+  res.render('form');
+});
